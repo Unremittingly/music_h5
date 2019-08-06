eda095b3c66d6a6efccec3766039fe99e13faf35
object <>
2019-08-06 09:33:01 +0800

    init

---
+++ src/components/bottom/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        bottom
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/page/home/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <Bottom></Bottom>
+        <div class="content">
+            content
+        </div>
+        <Header></Header>
+    </div>
+
+</template>
+
+<script>
+
+    import Bottom from '../../components/bottom/index'
+    import Header from '../../components/head/index'
+    export default {
+        name: "index",
+        components: {
+            Bottom,
+            Header
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
