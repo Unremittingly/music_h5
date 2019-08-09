59e03aa8ec57fb8cf8e969707057acc21707c00a
object <>
2019-08-09 13:30:04 +0800

    down load  fail

--- src/components/list/item.vue
+++ src/components/list/item.vue
@@ -5,7 +5,7 @@
             <div class="song-album"> {{item.album.albumname}}</div>
         </div>
         <div class="song-singer item-i" v-bind:data-mid="item.singer.mid" v-bind:data-id="item.singer.id">{{item.singer.name}}</div>
-        <div class="song-opera item-i"><i class="el-icon-download" @click="downLoad(item)"></i></div>
+        <div class="song-opera item-i"><a :href="downSrc" ><i class="el-icon-download" @click.stop="downLoad(item)"></i></a></div>
     </div>
 </template>
 
@@ -14,10 +14,16 @@
 
     export default {
         name: "item",
+        data(){
+            return {
+                downSrc:'',
+            }
+        },
         props: {
             item: {
                 type: Object,
                 default: () => {
+
                 }
             }
         },
@@ -37,14 +43,19 @@
 
             },
             downLoad(item) {
-                let id = item.id;
+                let id = item.songId;
 
                 getPlayUrl(id).then(function (data) {
+                    console.log('11');
+                    //a标签下载只能现在相对路径  如果是绝对路径会认为是一个url 无法下载 只能借助后台node 下载
+                    const ele = document.createElement('a');
+                    ele.setAttribute('href', data.data.data); //设置下载文件的url地址
+                    ele.setAttribute('download', item.name);//用于设置下载文件的文件名
+                    ele.click(function () {
+                        return false;
+                    });
                     if (data.data.code === '200') {
-                        const ele = document.createElement('a');
-                        ele.setAttribute('href', data.data.data); //设置下载文件的url地址
-                        ele.setAttribute('download', item.name);//用于设置下载文件的文件名
-                        ele.click();
+
                     }
                 })
             }
